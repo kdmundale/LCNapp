3e218015ec3fabca8adc692cf3745f2e457a1927
Kate Mundale <kdmundale192@stevenscollege.edu>
2021-03-16 14:41:41 -0400

    change to digital ocean db connection

--- db/index.js
+++ db/index.js
@@ -1,9 +1,20 @@
 const { Pool } = require('pg')
+const connectionString="postgres://gioaaflxktcavy:ee76ee17b037cd235a754fa08f840e525c652ae95eb46061d342b48481575889@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d3q5g6rp3m8srr"
+
+// const pool = new Pool({
+//   connectionString,
+// })
 
 const pool = new Pool({
-  host: 'localhost',
-  database: 'lcnapp_test',
-  password: '',
+  username: 'dodadmin',
+  host: 'db-postgresql-nyc3-84214-do-user-8838963-0.b.db.ondigitalocean.com',
+  database: 'defaultdb',
+  password: 'a5ruz097kqsk93xh',
+  port: 25060,
+  ssl:'true',
+  // host: 'localhost',
+  // database: 'lcnapp_test',
+  // password: '',
 })
 
 module.exports = {
