e159f3e62a996bc54082f1f9b658e3369bf49041
Kate Mundale <kdmundale192@stevenscollege.edu>
2021-03-16 15:56:17 -0400

    fix connection issues

--- db/index.js
+++ db/index.js
@@ -1,21 +1,24 @@
 const { Pool } = require('pg')
-const connectionString="postgres://gioaaflxktcavy:ee76ee17b037cd235a754fa08f840e525c652ae95eb46061d342b48481575889@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d3q5g6rp3m8srr"
-
-// const pool = new Pool({
-//   connectionString,
-// })
+const connectionString="postgresql://doadmin:show-password@db-postgresql-nyc3-84214-do-user-8838963-0.b.db.ondigitalocean.com:25060/defaultdb?"
 
 const pool = new Pool({
-  username: 'dodadmin',
-  host: 'db-postgresql-nyc3-84214-do-user-8838963-0.b.db.ondigitalocean.com',
-  database: 'defaultdb',
-  password: 'a5ruz097kqsk93xh',
-  port: 25060,
-  ssl:'true',
-  // host: 'localhost',
-  // database: 'lcnapp_test',
-  // password: '',
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+// const pool = new Pool({
+//   username: 'dodadmin',
+//   host: 'db-postgresql-nyc3-84214-do-user-8838963-0.b.db.ondigitalocean.com',
+//   database: 'defaultdb',
+//   password: 'a5ruz097kqsk93xh',
+//   port: 25060,
+//   ssl:'true',
+//   // host: 'localhost',
+//   // database: 'lcnapp_test',
+//   // password: '',
+// })
 
 module.exports = {
   async query(text, params) {
